7ff7d2d6f921acc0cf60a5f295a298f6c59854f9
hedanhua <592536849@qq.com>
2024-10-15 11:27:04 +0800

    11

---
+++ src/main/java/com/yz/GameMain.java
@@ -0,0 +1,31 @@
+package com.yz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableScheduling
+@EnableTransactionManagement
+@SpringBootApplication
+public class GameMain implements ApplicationListener<ContextClosedEvent> {
+	static ApplicationContext context;
+	
+	public static void main(String[] args) {
+		context = SpringApplication.run(GameMain.class, args);
+	}
+
+	public static <T> T getBean(Class<T> cls) {
+		return context.getBean(cls);
+	}
+
+	@Override
+	public void onApplicationEvent(ContextClosedEvent arg0) {
+
+	}
+	
+
+}
